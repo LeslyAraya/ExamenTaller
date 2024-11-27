ec3c2fcae386371c31aeb80feca6f7db7e0b8184
LeslyAraya <lesarayamor@est.utn.ac.cr>
2024-11-27 15:06:31 -0600

    Se programa el DTO de Inscripcion

--- ModeloInscripcion/Inscripcion.java
+++ ModeloInscripcion/Inscripcion.java
@@ -18,4 +18,39 @@ public class Inscripcion {
     private Participante participante;
     private Date fecha;
     private String asistencia;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getTaller() {
+        return Taller;
+    }
+
+    public Participante getParticipante() {
+        return participante;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public String getAsistencia() {
+        return asistencia;
+    }
+
+    public Inscripcion(int id, String Nombre, String Taller, Participante participante, Date fecha, String asistencia) {
+        this.id = id;
+        this.Nombre = Nombre;
+        this.Taller = Taller;
+        this.participante = participante;
+        this.fecha = fecha;
+        this.asistencia = asistencia;
+    }
+    
+    
 }
