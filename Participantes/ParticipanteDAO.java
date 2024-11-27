8494801afd9c476ad8df0f919e41d5ed08eb6ec1
fabiana <linunezgo@est.utn.ac.cr>
2024-11-27 15:14:23 -0600

    Se crean algunos DAO

--- Participantes/ParticipanteDAO.java
+++ Participantes/ParticipanteDAO.java
@@ -6,7 +6,13 @@ package Participantes;
 
 import Conexion.Conexion;
 import Modelo.DAO.DAO;
+import Modelo.Dao.Dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -20,12 +26,23 @@ public class ParticipanteDAO extends DAO<ParticipanteDTO>{
 
     @Override
     public boolean Agregar(Object dto) throws SQLException {
-       
+       String sql = "INSERT INTO participantes (id, nombre, telefono, correo) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, dto.getId());
+            statement.setString(2, dto.getNombre());
+            statement.setString(3, dto.getTelefono());
+            statement.setString(4, dto.getCorreo));
+            return statement.executeUpdate() > 0; 
+        }
     }
 
     @Override
     public boolean Eliminar(Object dto) throws SQLException {
-     
+     String sql = "DELETE FROM participante WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, (String) id);
+            return statement.executeUpdate() > 0;
+        }
     }
     
 }
