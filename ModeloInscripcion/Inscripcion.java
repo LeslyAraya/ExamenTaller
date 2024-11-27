917958babb1a06d7e7b99ad42b0cc2c1a67f6c48
Thyfanny Maryel Martínez Gómez <thmartinezgo@est.utn.ac.cr>
2024-11-27 14:11:06 -0600

    Se crea la clase inscipcion

---
+++ ModeloInscripcion/Inscripcion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModeloInscripcion;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Student
+ */
+public class Inscripcion {
+    private int id;
+    private String Nombre;
+    private String Taller;
+    private Participante participante;
+    private Date fecha;
+    private String asistencia;
+}
