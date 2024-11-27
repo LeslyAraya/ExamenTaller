fdfdbdbac532e6cdd1c0b58e644efc21e64eea0a
LeslyAraya <lesarayamor@est.utn.ac.cr>
2024-11-27 14:44:44 -0600

    Programacion de ParticipanteDAO

--- Participantes/ParticipanteDAO.java
+++ Participantes/ParticipanteDAO.java
@@ -4,10 +4,28 @@
  */
 package Participantes;
 
+import Conexion.Conexion;
+import Modelo.DAO.DAO;
+import java.sql.SQLException;
+
 /**
  *
  * @author Student
  */
-public class ParticipanteDAO extends {
+public class ParticipanteDAO extends DAO<ParticipanteDTO>{
+
+    public ParticipanteDAO(Conexion connection) {
+        super(connection);
+    }
+
+    @Override
+    public boolean Agregar(Object dto) throws SQLException {
+       
+    }
+
+    @Override
+    public boolean Eliminar(Object dto) throws SQLException {
+     
+    }
     
 }
